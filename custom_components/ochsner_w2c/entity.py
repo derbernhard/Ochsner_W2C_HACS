742d781df761081d28f3341775c47fc1aa765467
derbernhard <63771175+derbernhard@users.noreply.github.com>
2026-09-14 08:55:54 +0200

    Refactor Entity class to W2CEntity

--- custom_components/ochsner_w2c/entity.py
+++ custom_components/ochsner_w2c/entity.py
@@ -1,4 +1,7 @@
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-class Entity(CoordinatorEntity):
- _attr_has_entity_name=True
- def __init__(self,c,e,key):super().__init__(c);self._attr_unique_id=f"{e.unique_id}_{key}";self._attr_device_info={'identifiers':{('ochsner_w2c',e.unique_id)},'name':'Ochsner W2C','manufacturer':'Ochsner','model':'W2C'}
+class W2CEntity(CoordinatorEntity):
+    _attr_has_entity_name = True
+    def __init__(self, coordinator, entry, key):
+        super().__init__(coordinator)
+        self._attr_unique_id = f'{entry.unique_id}_{key}'
+        self._attr_device_info = {'identifiers': {('ochsner_w2c', entry.unique_id)}, 'name': 'Ochsner W2C', 'manufacturer': 'Ochsner', 'model': 'W2C'}
