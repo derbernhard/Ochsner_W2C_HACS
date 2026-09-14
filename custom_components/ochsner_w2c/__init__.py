bc4bfc93fca17897e4f49791a96a2d0ff0e95e97
derbernhard <63771175+derbernhard@users.noreply.github.com>
2026-09-14 08:53:15 +0200

    Refactor async_setup_entry and async_unload_entry

--- custom_components/ochsner_w2c/__init__.py
+++ custom_components/ochsner_w2c/__init__.py
@@ -1,8 +1,13 @@
-from homeassistant.const import CONF_USERNAME,CONF_PASSWORD
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from .api import W2CApi
-from .coordinator import Coordinator
 from .const import *
-async def async_setup_entry(hass,entry):
- c=Coordinator(hass,W2CApi(async_get_clientsession(hass),entry.data[CONF_GATEWAY_HOST],entry.data[CONF_TARGET_HOST],entry.data[CONF_USERNAME],entry.data[CONF_PASSWORD],entry.data.get(CONF_VERIFY_SSL,False)),entry.data.get(CONF_SCAN_INTERVAL,DEFAULT_SCAN_INTERVAL));await c.async_config_entry_first_refresh();entry.runtime_data=c;await hass.config_entries.async_forward_entry_setups(entry,PLATFORMS);return True
-async def async_unload_entry(hass,entry):return await hass.config_entries.async_unload_platforms(entry,PLATFORMS)
+from .coordinator import W2CCoordinator
+async def async_setup_entry(hass, entry):
+    api = W2CApi(hass, entry.data[CONF_W2C_HOST], entry.data[CONF_USERNAME], entry.data[CONF_PASSWORD])
+    coordinator = W2CCoordinator(hass, api, entry.data.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL), entry.data.get(CONF_MQTT_ENABLED, False), entry.data.get(CONF_MQTT_BASE_TOPIC, DEFAULT_MQTT_BASE_TOPIC))
+    await coordinator.async_config_entry_first_refresh()
+    entry.runtime_data = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+async def async_unload_entry(hass, entry):
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
