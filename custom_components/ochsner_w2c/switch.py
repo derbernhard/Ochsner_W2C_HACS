a83c1e0359264850bf2d51a7db6492b39228f918
derbernhard <63771175+derbernhard@users.noreply.github.com>
2026-09-22 14:46:42 +0200

    Refactor switch.py to add Boost functionality

--- custom_components/ochsner_w2c/switch.py
+++ custom_components/ochsner_w2c/switch.py
@@ -1,12 +1,50 @@
+"""Switch entities for Ochsner W2C."""
 from homeassistant.components.switch import SwitchEntity
+
+from .const import OID_WW_ACTUAL
 from .entity import W2CEntity
-async def async_setup_entry(hass,e,add): add([Manual(e.runtime_data,e)])
-class Manual(W2CEntity,SwitchEntity):
-    _attr_name='Warmwasser manuell'; _attr_icon='mdi:water-boiler'
-    def __init__(self,c,e): super().__init__(c,e,'water_manual')
+
+
+async def async_setup_entry(hass, entry, add_entities):
+    add_entities([
+        Boost(entry.runtime_data, entry),
+    ])
+
+
+class Boost(W2CEntity, SwitchEntity):
+    """Aktiviert den Warmwasser-Boost."""
+
+    _attr_name = "Warmwasser Boost"
+    _attr_icon = "mdi:water-boiler"
+
+    def __init__(self, coordinator, entry):
+        super().__init__(coordinator, entry, "ww_boost")
+
     @property
-    def is_on(self):
-        try:return int(self.coordinator.data.get('/1/2/7/107/0'))==2
-        except (TypeError,ValueError):return False
-    async def async_turn_on(self,**kw): await self.coordinator.async_write('/1/2/7/107/0',2)
-    async def async_turn_off(self,**kw): await self.coordinator.async_write('/1/2/7/107/0',1)
+    def is_on(self) -> bool:
+        return self.coordinator.boost_active
+
+    @property
+    def extra_state_attributes(self):
+        c = self.coordinator
+        low, high = c.boost_limits
+        try:
+            actual = float(c.data.get(OID_WW_ACTUAL))
+        except (TypeError, ValueError):
+            actual = None
+
+        return {
+            "boost_aktiv": c.boost_active,
+            "boost_ziel": c.boost_target,
+            "boost_fortschritt_pct": None if actual is None else round(min(100.0, actual / c.boost_target * 100.0), 1),
+            "ist_temperatur": actual,
+            "gespeicherter_sollwert": c.saved_setpoint,
+            "sollwert_min": low,
+            "sollwert_max": high,
+        }
+
+    async def async_turn_on(self, **kwargs):
+        await self.coordinator.async_boost_start()
+
+    async def async_turn_off(self, **kwargs):
+        await self.coordinator.async_boost_stop()
